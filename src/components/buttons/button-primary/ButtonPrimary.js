1bc95520e33f4e47904e27f61cb3d6a457dee9db
emranweb <emranbd77@gmail.com>
2021-08-19 20:10:57 +0600

    button

---
+++ src/components/buttons/button-primary/ButtonPrimary.js
@@ -0,0 +1,9 @@
+import ButtonPrimaryStyled from "./ButtonPrimary.style";
+
+import React from "react";
+
+export const ButtonPrimary = ({ children }) => {
+  return <ButtonPrimaryStyled>{children}</ButtonPrimaryStyled>;
+};
+
+export default ButtonPrimary;
---
+++ src/components/buttons/index.js
@@ -0,0 +1,8 @@
+// import button primary and secondary
+import ButtonPrimary from './button-primary/ButtonPrimary';
+import ButtonSecondary from './button-secondary/ButtonSecondary';
+
+export {
+    ButtonPrimary,
+    ButtonSecondary
+}
