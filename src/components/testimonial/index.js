c43e0857f3b4332e951af027ece5000a74a9ebcc
emranweb <emranbd77@gmail.com>
2021-09-04 16:25:12 +0600

    testitmonial

--- src/components/testimonial/index.js
+++ src/components/testimonial/index.js
@@ -1,13 +1,47 @@
-import React, { useState, useRef } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { P } from "../typography";
+import React, { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+//import { P } from "../typography";
 
 const Testimonial = () => {
-  const [index, setIndex] = useState(1);
-  const elementRef = useRef();
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState("");
+
   const ChangeTestimonialItem = (e) => {
-    console.log(elementRef);
+    console.log(e.target.dataset.index);
+    setIndex(e.target.dataset.index);
   };
+
+  const testimonialData = [
+    {
+      id: 0,
+      name: "Steve Collins",
+      company: "Artel Corp.",
+      content:
+        "1 Awesome template, well written code and looks great on any platform. The customer support is very helpful and as huge asset to this template. Overall a good design, that I am quite happy with.Awesome template, well written code and looks great on any platform. The customer support is very helpful and as huge asset to this template. Overall a good design, that I am quite happy with.",
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      id: 1,
+      name: "Steve Collins",
+      company: "Artel Corp.",
+      content:
+        "2 Awesome template, well written code and looks great on any platform. The customer support is very helpful and as huge asset to this template. Overall a good design, that I am quite happy with.Awesome template, well written code and looks great on any platform. The customer support is very helpful and as huge asset to this template. Overall a good design, that I am quite happy with.",
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      id: 2,
+      name: "Steve Collins",
+      company: "Artel Corp.",
+      content:
+        "3 Awesome template, well written code and looks great on any platform. The customer support is very helpful and as huge asset to this template. Overall a good design, that I am quite happy with.Awesome template, well written code and looks great on any platform. The customer support is very helpful and as huge asset to this template. Overall a good design, that I am quite happy with.",
+      img: "https://via.placeholder.com/150",
+    },
+  ];
+
+  useEffect(() => {
+    setData(testimonialData[index]);
+  }, [index]);
+
   return (
     <>
       <Container>
@@ -15,60 +49,21 @@ const Testimonial = () => {
           <Col lg={12}>
             <div className="testimonial-section">
               <div className="testimonial-content">
-                <div
-                  className="testimonial-content-item active"
-                  ref={elementRef}
-                >
-                  <P>
-                    Awesome template, well written code and looks great on any
-                    platform. The customer support is very helpful and as huge
-                    asset to this template. Overall a good design, that I am
-                    quite happy with.Awesome template, well written code and
-                    looks great on any platform. The customer support is very
-                    helpful and as huge asset to this template. Overall a good
-                    design, that I am quite happy with.
-                  </P>
-                </div>
-                <div className="testimonial-content-item">
-                  <P>
-                    Awesome template, well written code and looks great on any
-                    platform. The customer support is very helpful and as huge
-                    asset to this template. Overall a good design, that I am
-                    quite happy with.Awesome template, well written code and
-                    looks great on any platform. The customer support is very
-                    helpful and as huge asset to this template. Overall a good
-                    design, that I am quite happy with.
-                  </P>
-                </div>
-                <div className="testimonial-content-item">
-                  <P>
-                    Awesome template, well written code and looks great on any
-                    platform. The customer support is very helpful and as huge
-                    asset to this template. Overall a good design, that I am
-                    quite happy with.Awesome template, well written code and
-                    looks great on any platform. The customer support is very
-                    helpful and as huge asset to this template. Overall a good
-                    design, that I am quite happy with.
-                  </P>
-                </div>
+                <div>{data.content}</div>
               </div>
               <div className="testimonial-images">
-                <img
-                  src="https://via.placeholder.com/150"
-                  data-index="1"
-                  alt="person"
-                  onClick={(e) => ChangeTestimonialItem(e)}
-                />
-                <img
-                  src="https://via.placeholder.com/170"
-                  data-index="2"
-                  alt="person"
-                />
-                <img
-                  src="https://via.placeholder.com/180"
-                  data-index="3"
-                  alt="person"
-                />
+                {testimonialData.map((item) => {
+                  return (
+                    <div className="item_img" key={item.id}>
+                      <img
+                        src={item.img}
+                        alt="person"
+                        data-index={item.id}
+                        onClick={(e) => ChangeTestimonialItem(e)}
+                      />
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </Col>
